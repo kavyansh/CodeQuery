d861296998ebe03af585287f6d24208ff02ddbd0
Yash Srivastava <yashsrivastava722@gmail.com>
2024-12-15 22:45:59 +0530

    Assets and Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,29 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import localFont from "next/font/local";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const inter = localFont({
+  src: "./fonts/InterVF.ttf",
+  variable: "--font-inter",
+  weight: "100 200 300 400 500 600 700 800 900",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const spaceGroteskVF = localFont({
+  src: "./fonts/SpaceGroteskVF.ttf",
+  variable: "--font-space-grotesk",
+  weight: "300 400 500 600 700",
+  // variable -- will be chosen as variable
+  // className will be default
 });
 
 export const metadata: Metadata = {
-  title: "List Overflow",
+  // Useful for SEO
+  title: "Code Query",
   description:
     "A collaborative platform for developers to ask and answer technical questions, share knowledge, and build a community around coding solutions.",
+  icons: {
+    icon: "/images/site-logo.svg",
+  },
 };
 
 export default function RootLayout({
@@ -26,7 +34,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.className} ${spaceGroteskVF.variable} antialiased`}
       >
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
-const Home = () => <h1 className="h1-bold">Welcome to nextjs</h1>;
-
-export default Home;
+export default function Home() {
+  return (
+    <>
+      <h1 className="h1-bold">Welcome to nextjs</h1>
+      <h1 className="h1-bold font-space-grotesk">Welcome to nextjs - inter</h1>
+    </>
+  );
+}
